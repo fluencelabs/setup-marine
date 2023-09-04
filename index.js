47339915415022202640227368a392f9943ee6cd
Anatolios Laskaris <github_me@nahsi.dev>
2023-09-04 14:35:32 +0300

    chore: Download marine artifact if present (#9)
    * Download marine binary from artifacts
    
    * Issue a warning when artifact is not found but continue
    
    * Add to inputs
    
    * Fix
    
    * Debug release
    
    * do not fail fast
    
    * Fix
    
    * Run on ubuntu
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Add setup artifact test
    
    * Fix
    
    * fix
    
    * Use @github/artifact
    
    * Add lib
    
    * Fix
    
    * Exec
    
    * Update

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const core = require("@actions/core");
 const tc = require("@actions/tool-cache");
-const { promisify } = require("util");
-const { exec } = require("child_process");
-const { chmod } = require("fs");
 const { Octokit } = require("@octokit/rest");
-
+const { create } = require("@actions/artifact");
+const path = require("path");
+const fs = require("fs");
+const { execSync } = require('child_process');
 const DOWNLOAD_URL = "https://github.com/fluencelabs/marine/releases/download/";
 const SUPPORTED_PLATFORMS = ["linux-x86_64", "darwin-x86_64"];
 
@@ -19,60 +19,102 @@ function guessPlatform() {
   return platformMappings[platform] || platform;
 }
 
-async function run() {
-  try {
-    const platform = guessPlatform();
-    if (!SUPPORTED_PLATFORMS.includes(platform)) {
-      throw new Error(`Unsupported platform: ${platform}`);
-    }
+async function downloadArtifact(artifactName) {
+  const artifactClient = create();
+  const tempDirectory = process.env.RUNNER_TEMP; // GitHub Actions' temp directory
 
-    let version = core.getInput("version");
-    if (version === "latest") {
-      const octokit = new Octokit();
-      const releases = await octokit.repos.listReleases({
-        owner: "fluencelabs",
-        repo: "marine",
-      });
-      const latestRelease = releases.data.find((release) =>
-        release.tag_name.startsWith("marine-v")
-      );
-      if (!latestRelease) {
-        throw new Error("No marine release found");
-      }
-      version = latestRelease.tag_name.replace(/^marine-v/, "");
-      core.info(`Latest marine release is v${version}`);
-    } else {
-      version = version.replace(/^v/, "");
-    }
-
-    const filename = `marine`;
-    const downloadUrl =
-      `${DOWNLOAD_URL}marine-v${version}/${filename}-${platform}`;
-    const cachedPath = tc.find("marine", version, platform);
-
-    let marinePath;
-    if (!cachedPath) {
-      const downloadPath = await tc.downloadTool(downloadUrl);
-      marinePath = await tc.cacheFile(
-        downloadPath,
-        filename,
-        "marine",
-        version,
-      );
-    } else {
-      marinePath = cachedPath;
-    }
-
-    core.addPath(marinePath);
-    await promisify(chmod)(`${marinePath}/marine`, 0o755);
-
-    await promisify(exec)("marine --version");
-    core.info(
-      `marine v${version} for ${platform} has been set up successfully`,
-    );
-  } catch (error) {
-    core.setFailed(error.message);
+  if (!tempDirectory) {
+    throw new Error("Temp directory not found");
   }
+
+  // Create a unique directory for our action within the temp directory
+  const uniqueTempDir = path.join(
+    tempDirectory,
+    `marine-artifact-${Date.now()}`,
+  );
+  fs.mkdirSync(uniqueTempDir, { recursive: true });
+
+  const downloadResponse = await artifactClient.downloadArtifact(
+    artifactName,
+    uniqueTempDir,
+  );
+
+  const marineBinaryPath = path.join(downloadResponse.downloadPath, "marine");
+
+  // Check if the marine binary exists at the expected location
+  if (fs.existsSync(marineBinaryPath)) {
+    return downloadResponse.downloadPath; // Return the directory containing the marine binary
+  } else {
+    throw new Error(
+      `Expected marine binary not found in the artifact at path: ${marineBinaryPath}`,
+    );
+  }
+}
+
+async function getLatestVersionFromReleases() {
+  const octokit = new Octokit();
+  const releases = await octokit.repos.listReleases({
+    owner: "fluencelabs",
+    repo: "marine",
+  });
+  const latestRelease = releases.data.find((release) =>
+    release.tag_name.startsWith("marine-v")
+  );
+  if (!latestRelease) {
+    throw new Error("No latest marine release found");
+  }
+  return latestRelease.tag_name.replace(/^marine-v/, "");
+}
+
+async function setupBinary(marinePath) {
+    core.addPath(marinePath);
+    await fs.promises.chmod(`${marinePath}/marine`, 0o755);
+    execSync(`${marinePath}/marine --version`, { stdio: 'inherit' });
+}
+
+async function run() {
+    try {
+        const platform = guessPlatform();
+        if (!SUPPORTED_PLATFORMS.includes(platform)) {
+            throw new Error(`Unsupported platform: ${platform}`);
+        }
+
+        const artifactName = core.getInput("artifact-name");
+        let marinePath;
+
+        if (artifactName) {
+            try {
+                marinePath = await downloadArtifact(artifactName);
+                await setupBinary(marinePath);
+                return;
+            } catch (_error) {
+                core.warning(`Failed to download artifact with name ${artifactName}. Fallback to releases.`);
+            }
+        }
+
+        let version = core.getInput("version");
+        if (version === "latest") {
+            version = await getLatestVersionFromReleases();
+            core.info(`Latest marine release is v${version}`);
+        } else {
+            version = version.replace(/^v/, "");
+        }
+
+        const filename = `marine`;
+        const downloadUrl = `${DOWNLOAD_URL}marine-v${version}/${filename}-${platform}`;
+        const cachedPath = tc.find("marine", version, platform);
+
+        if (!cachedPath) {
+            const downloadPath = await tc.downloadTool(downloadUrl);
+            marinePath = await tc.cacheFile(downloadPath, filename, "marine", version);
+        } else {
+            marinePath = cachedPath;
+        }
+
+        await setupBinary(marinePath);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
 }
 
 run();
